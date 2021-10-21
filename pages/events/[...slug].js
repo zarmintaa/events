0024cec411b4414ecd4c6296f929856b074f8d9d
HuaaXZ <59224888+zarmintaa@users.noreply.github.com>
2021-10-21 20:47:11 +0700

    add filter search, refactor code, add error handling

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,8 +1,67 @@
+import { useRouter } from "next/router";
+import EventList from "../../components/events/event-list";
+import ResultsTitle from "../../components/events/results-title";
+import { getFilteredEvents } from "../../dummy-data";
+import { Fragment } from "react";
+import Button from "../../components/ui/button";
+import ErrorAlert from "../../components/error-alert/error-alert";
+
 const FilteredEventsPage = () => {
+  const router = useRouter();
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filterYear = filterData.at(0);
+  const filterMonth = filterData.at(1);
+
+  const numYear = +filterYear;
+  const numMonth = +filterMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid Filter Please Adjust Your Value</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show all Event</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid Filter Please Adjust Your Value</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show all Event</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const date = new Date(numYear, numMonth - 1);
+
   return (
-    <div>
-      <h1>Filtered Events</h1>
-    </div>
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
   );
 };
 
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -4,6 +4,8 @@ import { Fragment } from "react";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventContent from "./../../components/event-detail/event-content";
 import EventLogistics from "./../../components/event-detail/event-logistics";
+import ErrorAlert from "../../components/error-alert/error-alert";
+import Button from "./../../components/ui/button";
 
 const EventDetailPage = () => {
   const router = useRouter();
@@ -11,7 +13,17 @@ const EventDetailPage = () => {
   const event = getEventById(eventId);
 
   if (!event) {
-    return <p>No Event Found</p>;
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No Event Found</p>
+        </ErrorAlert>
+
+        <div className="center">
+          <Button link="/events">Browse All Events</Button>
+        </div>
+      </Fragment>
+    );
   }
 
   return (
