fe0beb8c4e0af256ed6afb8b2bbfb74d63fbbe64
HuaaXZ <59224888+zarmintaa@users.noreply.github.com>
2021-10-29 17:18:41 +0700

    Update notification-context.js

--- store/notification-context.js
+++ store/notification-context.js
@@ -1,32 +1,49 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const NotificationContext = createContext({
-  notification: null,
+  notification: null, // { title, message, status }
   showNotification: function (notificationData) {},
   hideNotification: function () {},
 });
 
-export const NotificationContextProvider = (props) => {
+export function NotificationContextProvider(props) {
   const [activeNotification, setActiveNotification] = useState();
 
-  const showNotificationHandler = (notificationData) => {
-    setActiveNotification(notificationData);
-  };
-  const hideNotificationHandler = () => {
-    setActiveNotification(null);
-  };
+  useEffect(() => {
+    if (
+      activeNotification &&
+      (activeNotification.status === "success" ||
+        activeNotification.status === "error")
+    ) {
+      const timer = setTimeout(() => {
+        setActiveNotification(null);
+      }, 3000);
 
-  const myContext = {
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [activeNotification]);
+
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData);
+  }
+
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  const context = {
     notification: activeNotification,
     showNotification: showNotificationHandler,
     hideNotification: hideNotificationHandler,
   };
 
   return (
-    <NotificationContext.Provider value={myContext}>
+    <NotificationContext.Provider value={context}>
       {props.children}
     </NotificationContext.Provider>
   );
-};
+}
 
 export default NotificationContext;
